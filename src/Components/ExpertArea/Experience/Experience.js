3357244ff064b6c8146d5560c8759218e28d2973
SaqlineShabbir <saqline5869@gmail.com>
2024-06-10 10:23:29 +0600

    changes

--- src/Components/ExpertArea/Experience/Experience.js
+++ src/Components/ExpertArea/Experience/Experience.js
@@ -1,47 +1,40 @@
-import { Fade } from 'react-reveal';
-import './Experience.css';
+import "./Experience.css";
 const Experience = () => {
   return (
-    <Fade right>
-      <div className="text-gray-700 experience-main mt-10">
-        <div className="pb-5  pe-5 text-gray-700">
-          <p>2021 - Present</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            quis.
-          </p>
-          <hr />
-          <h4>MERN Stack Developer</h4>
-        </div>
-        <div className="text-gray-700">
-          <p>2021 - Present</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            quis.
-          </p>
-          <hr />
-          <h4>MERN Stack Developer</h4>
-        </div>
-        <div className="pe-5 text-gray-700">
-          <p>2021 - Present</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            quis.
-          </p>
-          <hr />
-          <h4>MERN Stack Developer</h4>
-        </div>
-        <div className="text-gray-700">
-          <p>2021 - Present</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            quis.
-          </p>
-          <hr />
-          <h4>MERN Stack Developer</h4>
-        </div>
+    <div className="text-gray-700 experience-main mt-10">
+      <div className="pb-5  pe-5 text-gray-700">
+        <p>2021 - Present</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quis.
+        </p>
+        <hr />
+        <h4>MERN Stack Developer</h4>
       </div>
-    </Fade>
+      <div className="text-gray-700">
+        <p>2021 - Present</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quis.
+        </p>
+        <hr />
+        <h4>MERN Stack Developer</h4>
+      </div>
+      <div className="pe-5 text-gray-700">
+        <p>2021 - Present</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quis.
+        </p>
+        <hr />
+        <h4>MERN Stack Developer</h4>
+      </div>
+      <div className="text-gray-700">
+        <p>2021 - Present</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quis.
+        </p>
+        <hr />
+        <h4>MERN Stack Developer</h4>
+      </div>
+    </div>
   );
 };
 
